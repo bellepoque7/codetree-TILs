6cc919162f735fee269e72281f591d72811ceb82
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2024-02-06 18:20:06 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240206/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,15 @@
+import sys
+read = sys.stdin.readline
+
+n, m = map(int, read().split())
+# 1. 한 수가 다른 수의 배수일 경우
+# 큰 수가 최소 공배수이다.
+
+# 2. 그렇지 않을 경우 
+# 두 수를 동시에 나눌 수 있는 수를 찾은뒤 각 수를 최대공약수로 나누어 몫을 곱한다. 
+
+for i in range(2,100):
+    if n%i ==0 and  m%i == 0:
+        # print(i)
+        k = i
+print(n//k * m//k * k)
