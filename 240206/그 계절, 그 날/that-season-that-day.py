e14616bb81bea56b3ae25b056af11e77260b523f
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2024-02-06 19:11:13 +0900

    Title: 그 계절, 그 날, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240206/그 계절, 그 날/that-season-that-day.py
+++ 240206/그 계절, 그 날/that-season-that-day.py
@@ -14,7 +14,7 @@ Y, M, D = map(int, read().split())
 # 단 4의 배수면서 100의 배수면 28일까지
 # 단 4의 배수이서 100의 배수지만 400의 배수이면 29일까지
 # 나머지는 29일까지
-# Y, M, D = 1999, 2, 29
+# Y, M, D = 200, 2, 29
 
 Month = ''
 Flag = False
@@ -42,14 +42,12 @@ elif M in (4,6,9,11):
 elif M == 2:
     if Y%4 ==0 and Y%100 == 0 and Y%400 == 0 and 1<= D <= 29:
         Flag = True
-    elif Y% 4 ==0 and Y%100 == 0 and 1<= D <= 28:
+
+    if Y% 4 ==0 and Y%100 == 0 and 1<= D <= 28:
         Flag = True
-    elif Y% 4 ==0 and 1<=D <=29:
+    
+    if Y %4 !=0 and 1<= D<=28:
         Flag = True
-    elif Y% 4 !=0 and 1<= D <= 28:
-        Flag = True
-    else:
-        pass
 else:
     pass
 
