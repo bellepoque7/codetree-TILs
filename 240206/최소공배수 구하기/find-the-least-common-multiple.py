404021dc787159d1a5d1ad43f01b2cda4a07abd8
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2024-02-06 18:21:04 +0900

    Title: 최소공배수 구하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240206/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240206/최소공배수 구하기/find-the-least-common-multiple.py
@@ -8,6 +8,7 @@ n, m = map(int, read().split())
 # 2. 그렇지 않을 경우 
 # 두 수를 동시에 나눌 수 있는 수를 찾은뒤 각 수를 최대공약수로 나누어 몫을 곱한다. 
 
+k = max(n,m)
 for i in range(2,100):
     if n%i ==0 and  m%i == 0:
         # print(i)
