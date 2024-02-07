8295ea66aa39a1ff2f4527014e48a3feb9514b4e
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2024-02-07 14:35:03 +0900

    Title: 나누고 빼면서 합하기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -0,0 +1,15 @@
+import sys
+read = sys.stdin.readline
+n, m = map(int, read().split())
+my_list = list(map(int, input().split()))
+my_sum = 0
+for i in range(n):
+    my_sum += my_list[m]
+    my_sum += 1
+    if m == 1:
+        break
+    if m % 2 == 0 :
+        m = m//2
+    else:
+        m -= 1
+print(my_sum)
