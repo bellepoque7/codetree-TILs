f81ac8a7bcd486aec04e536eae9c607ff7c0f8b9
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2024-02-07 14:17:09 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240207/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240207/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -2,16 +2,12 @@ ipt_str = input()
 obj_str = input()
 Flag = False
 idx = -1
-#tadada
-#da
-
 for i in range(len(ipt_str)):
-    if Flag:
-        break
     for j in range(len(obj_str)):
-        if ipt_str[i: i+len(ipt_str)] == obj_str:
+        if ipt_str[i: i+len(obj_str)] == obj_str:
             idx = i
             Flag = True
             break
-    
+    if Flag:
+        break
 print(idx)
